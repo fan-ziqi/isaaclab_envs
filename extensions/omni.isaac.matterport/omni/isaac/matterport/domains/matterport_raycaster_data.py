d1a53e6acbb8d1cfa25cab821bb3dd308488e634
Pascal <roth.pascal@outlook.de>
2024-04-29 15:42:03 +0200

    forgotten files

---
+++ extensions/omni.isaac.matterport/omni/isaac/matterport/domains/matterport_raycaster_data.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2024 ETH Zurich (Robotic Systems Lab)
+# Author: Pascal Roth
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from __future__ import annotations
+
+import torch
+
+from omni.isaac.orbit.sensors.ray_caster import RayCasterData
+
+
+class MatterportRayCasterData(RayCasterData):
+    ray_class_ids: torch.Tensor = None
+    """The class ids for each ray hit."""
